e7db9719aa61ffd12da38c74ba534e87ff2dee5e
Taka005 <takanosumika.main@gmail.com>
2022-05-07 16:09:59 +0900

    少し修正

--- module/commands/avater.js
+++ module/commands/avater.js
@@ -2,13 +2,13 @@ async function avatar(message,client){
     const config = require("../../config.json")
     if (message.content.startsWith(`${config.prefix}avatar`)) {
         if(message.content === `${config.prefix}avatar`){
-            message.channel.send(message.author.avatarURL({ format: 'png', dynamic: true, size: 1024 }) || "https://cdn.discordapp.com/embed/avatars/0.png");
+            message.reply(message.author.avatarURL({ format: 'png', dynamic: true, size: 1024 }) || "https://cdn.discordapp.com/embed/avatars/0.png");
             return;
         }
         try{
           const args = message.content.split(" ").slice(1);
           let user = await client.users.fetch(args[0])
-          message.channel.send(user.avatarURL({ format: 'png', dynamic: true, size: 1024 }) || "https://cdn.discordapp.com/embed/avatars/0.png")
+          message.reply(user.avatarURL({ format: 'png', dynamic: true, size: 1024 }) || "https://cdn.discordapp.com/embed/avatars/0.png")
             .catch(()=>{return message.reply("アイコンが取得出来ませんでした")})
         }catch(err){
           message.reply("ユーザーが取得できませんでした")
