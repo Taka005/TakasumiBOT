7462c23fdeff518ecabef4d44e5ee7179bc818bf
Taka005 <01taka005@gmail.com>
2022-05-16 17:25:20 +0900

    修正

--- module/antilinks.js
+++ module/antilinks.js
@@ -4,7 +4,7 @@ function antilink(message,client){
   if(!message.channel.type === 'GUILD_TEXT' || message.author.bot) return;  
   if(message.content.match("https://") ||message.content.match("discord.gg") ||message.content.match("www.")){
       client.users.cache.get(message.author.id).send('あなたの送信したDiscordの招待リンク、宣伝チャンネル等以外で送信していませんか？')
-        .catch(()=>message.reply("あなたの送信したDiscordの招待リンク、宣伝チャンネル等以外で送信していませんか？"))
+        .catch(()=>message.reply("あなたの送信したリンクアドレス、宣伝チャンネル等以外や危険な物を送信していませんか？"))
     }
 }
 
