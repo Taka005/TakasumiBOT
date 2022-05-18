59311b77dce633010fda279689ce9d123948f528
Taka005 <01taka005@gmail.com>
2022-05-18 17:54:15 +0900

    少し変更

--- module/openmessage.js
+++ module/openmessage.js
@@ -2,7 +2,9 @@ async function openmessage(message,client){
   const config = require("../../config.json");
   if(!message.channel.type === 'GUILD_TEXT' || message.author.bot) return;  
   if(message.content.match("https://discord.com/channels/"+/[0-9]{18}/+"/"+/[0-9]{18}/+"/"+/[0-9]{18}/)){
-
+    const url = message.content.match(/[0-9]{18}/g);
+    const channel = client.channels.cache.get(url[1]);
+    const message = channel.messages.fetch(url[2]);
   }
 }
 
