62ef3378a4e1b53e00826ca22ea062356b54f24b
Taka005 <01taka005@gmail.com>
2022-05-18 20:48:31 +0900

    ほとんど完成

--- module/openmessage.js
+++ module/openmessage.js
@@ -5,6 +5,21 @@ async function openmessage(message,client){
     const url = message.content.match(/[0-9]{18}/g);
     const channel = client.channels.cache.get(url[1]);
     const message = channel.messages.fetch(url[2]);
+    if(!url[0]&& !url[1]&& !url[2]) return;
+    message.channel.send({
+      embeds:[{
+        color: "WHITLE",
+        author: {
+          name: `${message.author.tag}`,
+          icon_url: message.author.avatarURL()||"https://cdn.discordapp.com/embed/avatars/0.png",
+        },
+        description: `${message.content}`,
+        footer: {
+          text: `${message.channel.parent.name}:${message.channel.name}`
+        },
+        timestamp: message.createdAt
+      }]
+    });
   }
 }
 
