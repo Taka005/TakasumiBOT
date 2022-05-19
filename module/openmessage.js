42fd784832ddbd4e5fd634c65381fe56df0ad96f
Taka005 <01taka005@gmail.com>
2022-05-19 13:26:58 +0900

    修正

--- module/openmessage.js
+++ module/openmessage.js
@@ -1,22 +1,22 @@
 async function openmessage(message,client){
   if(!message.channel.type === 'GUILD_TEXT' || message.author.bot) return;  
-  if(message.content.match("https://discord.com/channels/"+/[0-9]{18}/+"/"+/[0-9]{18}/+"/"+/[0-9]{18}/)){
-    const url = message.content.match(/[0-9]{18}/g);
-    const channel = client.channels.cache.get(url[1]);
-    const message = channel.messages.fetch(url[2]);
-    if(!url[0]&& !url[1]&& !url[2]) return;
+  if(message.content.match("https://discord.com/channels/")){
+    const url = message.content.match(/\d{18}/g);
+    const channel = await client.channels.cache.get(url[1]);
+    const msg = await channel.messages.fetch(url[2]);
+    if(!url.length==3) return;
     message.channel.send({
       embeds:[{
         color: "WHITLE",
         author: {
-          name: `${message.author.tag}`,
-          icon_url: message.author.avatarURL()||"https://cdn.discordapp.com/embed/avatars/0.png",
+          name: `${msg.author.tag}`,
+          icon_url: msg.author.avatarURL()||"https://cdn.discordapp.com/embed/avatars/0.png",
         },
-        description: `${message.content}`,
+        description: `${msg.content}`,
         footer: {
-          text: `${message.channel.parent.name}:${message.channel.name}`
+          text: `${msg.channel.parent.name}:${msg.channel.name}`
         },
-        timestamp: message.createdAt
+        timestamp: msg.createdAt
       }]
     });
   }
