21f6600ce80390910c4579fbde5d30ee74ac898d
Taka005 <01taka005@gmail.com>
2022-05-24 23:37:48 +0900

    半分完成

---
+++ module/commands/support.js
@@ -0,0 +1,77 @@
+function support(message,client){
+  const config = require("../../config.json");
+  if(message.content.startsWith(`${config.prefix}support`)){
+    const args = message.content.slice(9);
+    if(message.content === `${config.prefix}support`) return message.reply("報告内容を記入してください");
+    client.users.cache.get(message.author.id).send(`${args}についてサポートサーバーに報告しました。\n後日DMにて回答をお送りいたします\nご報告ありがとうございました\nサポートサーバー:https://discord.gg/GPs3npB63m`)
+      .catch(()=>message.reply(`${args}について報告しました。\n後日DMにて回答をお送りいたしますので、DMでメッセージを送信可能にしてください\nご報告ありがとうございました\nサポートサーバー:https://discord.gg/GPs3npB63m`))
+    //サポートサーバーへ送信
+    if(!message.attachments.first()){
+      client.channels.cache.get("947484748773736538").send({//添付ファイルなし
+        embeds:[{
+          color: "WHITE",
+          author: {
+            name: `${message.author.tag}:${message.author.id}`,
+            icon_url: message.author.avatarURL()||"https://cdn.discordapp.com/embed/avatars/0.png",
+          },
+          description: args || "メッセージ内容がありません",
+          footer: {
+            icon_url: message.guild.iconURL(),
+            text: message.guild.name
+          },
+          timestamp: new Date()
+        }]
+      });
+    }else if(message.attachments.first().height && message.attachments.first().width){
+      const attachment = message.attachments.map(attachment => attachment.url)
+      client.channels.cache.get("947484748773736538").send({//添付ファイルあり(画像)
+        embeds:[{
+          color: "WHITE",
+          author: {
+            name: `${message.author.tag}:${message.author.id}`,
+            icon_url: message.author.avatarURL()||"https://cdn.discordapp.com/embed/avatars/0.png",
+          },
+          description: args || "メッセージ内容がありません",
+          image: {
+            url: attachment[0]
+          },
+          footer: {
+            icon_url: message.guild.iconURL(),
+            text: message.guild.name
+          },
+          timestamp: new Date()
+        }]
+      });
+    }else{
+      const attachment = message.attachments.map(attachment => attachment.url)
+      client.channels.cache.get("947484748773736538").send({//添付ファイルあり(画像以外)
+        embeds:[{
+          color: "WHITE",
+          author: {
+            name: `${message.author.tag}:${message.author.id}`,
+            icon_url: message.author.avatarURL()||"https://cdn.discordapp.com/embed/avatars/0.png",
+          },
+          description: args || "メッセージ内容がありません",
+          footer: {
+            icon_url: message.guild.iconURL(),
+            text: message.guild.name
+          },
+          fields: [
+            {
+              name: "**添付ファイル**",
+              value: `${attachment[0]}`
+            }
+          ],
+          timestamp: new Date()
+        }]
+      });
+    }
+    return;
+  }
+  
+  if(message.content.startsWith(`${config.prefix}back`)){
+
+  }
+}
+
+module.export = support
