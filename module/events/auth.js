fc29ad38e567209d1c29954fa6ee3725a0d35488
Taka005 <01taka005@gmail.com>
2022-05-25 22:33:43 +0900

    修正2

--- module/events/auth.js
+++ module/events/auth.js
@@ -3,7 +3,7 @@ async function auth(interaction){
     const role_id = interaction.customId.match(/\d{18}/);
     if(interaction.member.roles.cache.has(role_id)) return await interaction.reply({content: "既に役職を付与済みです",ephemeral: true});
     const role = interaction.member.roles.cache.get(role_id);
-    if(!role) return;
+    if(!role) return interaction.reply("設定されたロールが無効です");
     await interaction.member.roles.add(role)
       .then(()=>{interaction.reply({content: "認証しました",ephemeral: true})})
       .catch(()=>{interaction.reply({content: "認証に失敗しました...\nbotの権限、またはロールがしっかりと設定されてることを確認してください\n原因が解決できない場合はサポートサーバーまでよろしくお願いします\nhttps://discord.gg/GPs3npB63m",ephemeral: true})})
