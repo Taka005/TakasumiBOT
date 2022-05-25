7e2aa447a74771d471ff6716d263cf32bb16632c
Taka005 <01taka005@gmail.com>
2022-05-25 22:28:18 +0900

    test

--- module/events/auth.js
+++ module/events/auth.js
@@ -5,8 +5,8 @@ async function auth(interaction){
     const role = interaction.member.roles.cache.get(role_id);
     if(!role) return;
     await interaction.member.roles.add(role)
-      .then(()=>await interaction.reply({content: "認証しました",ephemeral: true}))
-      .catch(()=>await interaction.reply({content: "認証に失敗しました...\nbotの権限、またはロールがしっかりと設定されてることを確認してください\n原因が解決できない場合はサポートサーバーまでよろしくお願いします\nhttps://discord.gg/GPs3npB63m",ephemeral: true}))
+      .then(()=>{await interaction.reply({content: "認証しました",ephemeral: true})})
+      .catch(()=>{await interaction.reply({content: "認証に失敗しました...\nbotの権限、またはロールがしっかりと設定されてることを確認してください\n原因が解決できない場合はサポートサーバーまでよろしくお願いします\nhttps://discord.gg/GPs3npB63m",ephemeral: true})})
     return;
   }
 }
