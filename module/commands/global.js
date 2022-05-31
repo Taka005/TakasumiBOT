287bbab88af02084fb8580d5fde42dee15b915ef
Taka005 <01taka005@gmail.com>
2022-05-31 17:46:21 +0900

    修正

--- module/commands/global.js
+++ module/commands/global.js
@@ -9,19 +9,19 @@ async function global(message,client){
     }else{
       message.channel.setTopic("GLOBAL")
         .then(()=>client.channels.cache.filter(channel => channel.topic == "GLOBAL")
-          .forEach((channel) => {
-            channel.send({
-              embeds:[{
-                color: "WHITE",
-                author: {
-                  name: message.guild.name,
-                  icon_url: message.guild.iconURL()
-                },
-                description: `グローバルチャットに新しいサーバーが参加しました！\n みんなで挨拶してみましょう！\n\n※チャットを利用した場合、[利用規約](http://takabot.f5.si/terms.html)に同意されたことになります。必ずご確認ください`,
-                timestamp: new Date()
-              }]
-            });
-          })
+                    .forEach((channel) => {
+                      channel.send({
+                        embeds:[{
+                          color: "WHITE",
+                          author: {
+                            name: message.guild.name,
+                            icon_url: message.guild.iconURL() || "https://cdn.discordapp.com/embed/avatars/0.png"
+                          },
+                          description: `グローバルチャットに新しいサーバーが参加しました！\n みんなで挨拶してみましょう！\n\n※チャットを利用した場合、[利用規約](http://takabot.f5.si/terms.html)に同意されたことになります。必ずご確認ください`,
+                          timestamp: new Date()
+                        }]}
+                      );
+                    })
         )
         .catch(()=>message.channel.send("グローバルチャットへの接続に失敗しました...\nBOTの権限等を確認してください\nサポートサーバー:https://discord.gg/GPs3npB63m"))
     }
