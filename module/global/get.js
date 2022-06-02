7d518ccd0ecd9668625519956ad1183e34df4a91
Taka005 <takanosumika.main@gmail.com>
2022-06-02 15:15:25 +0900

    修正２

--- module/global/get.js
+++ module/global/get.js
@@ -14,7 +14,7 @@ function get(message,client){
           },
           description: msg.message.content,
           footer: {
-            text: `${msg.guild.name}<${message.author.username}>`,
+            text: `${msg.guild.name} <${message.author.username}>`,
             icon_url:msg.guild.iconURL ||"https://cdn.discordapp.com/embed/avatars/0.png"
           },
           timestamp: new Date()
@@ -29,15 +29,15 @@ function get(message,client){
         embeds:[{
           color: WHITE,
           author: {
-            name: msg.author.username + msg.author.discriminator,
+            name: `${msg.author.username}${msg.author.discriminator}`,
             icon_url: msg.author.avatarURL ||"https://cdn.discordapp.com/embed/avatars/0.png",
           },
           description:msg.message.content,
           image: {
-            url: msg.attachments[0].attachment.url
+            url: msg.attachments.url
           },
           footer: {
-            text: msg.guild.name,
+            text: `${msg.guild.name} <${message.author.username}>`,
             icon_url:msg.guild.iconURL ||"https://cdn.discordapp.com/embed/avatars/0.png"
           },
           timestamp: new Date()
@@ -52,18 +52,18 @@ function get(message,client){
         embeds:[{
           color: WHITE,
           author: {
-            name: msg.author.username + msg.author.discriminator,
+            name: `${msg.author.username}${msg.author.discriminator}`,
             icon_url: msg.author.avatarURL ||"https://cdn.discordapp.com/embed/avatars/0.png",
           },
           description:msg.message.content,
           footer: {
-            text: msg.guild.name,
+            text: `${msg.guild.name} <${message.author.username}>`,
             icon_url:msg.guild.iconURL ||"https://cdn.discordapp.com/embed/avatars/0.png"
           },
           fields: [
             {
               name: "**添付ファイル**",
-              value: msg.attachments[0].attachment.url
+              value: msg.attachments.url
             }
           ],
           timestamp: new Date()
