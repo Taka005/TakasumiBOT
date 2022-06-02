a4b6c9593162c1055641dbf894c237c28cf390b5
Taka005 <01taka005@gmail.com>
2022-06-02 12:27:41 +0900

    global変更

--- module/global/global.js
+++ module/global/global.js
@@ -1,77 +1,87 @@
 async function global(message,client){
   if(message.author.bot || message.channel.topic !== "==GLOBAL==") return;
-    if(!message.attachments.first()){
-      client.channels.cache.filter(channel => channel.topic == "==GLOBAL==").forEach((channel) =>{
-        channel.send({//添付ファイルなし
-          embeds:[{
-            color: message.member.displayHexColor,
-            author: {
-              name: message.author.tag,
-              icon_url: message.author.avatarURL()||"https://cdn.discordapp.com/embed/avatars/0.png",
-            },
-            description: message.content,
-            footer: {
-              text: message.guild.name,
-              icon_url:message.guild.iconURL() ||"https://cdn.discordapp.com/embed/avatars/0.png"
-            },
-            timestamp: new Date()
-          }]}
-        );
+
+  if(message.content.match("discord.com/invite") ||message.content.match("discord.gg")){
+    return message.reply({
+      embeds:[{
+        color: "RED",
+        description: "申し訳御座いませんが、グローバルチャットで\n招待リンクを送信することは禁止されています",
+      }]
+    })
+  }
+
+  if(!message.attachments.first()){
+    client.channels.cache.filter(channel => channel.topic == "==GLOBAL==").forEach((channel) =>{
+      channel.send({//添付ファイルなし
+        embeds:[{
+          color: message.member.displayHexColor,
+          author: {
+            name: message.author.tag,
+            icon_url: message.author.avatarURL()||"https://cdn.discordapp.com/embed/avatars/0.png",
+          },
+          description: message.content,
+          footer: {
+            text: message.guild.name,
+            icon_url:message.guild.iconURL() ||"https://cdn.discordapp.com/embed/avatars/0.png"
+          },
+          timestamp: new Date()
+        }]
       });
-      message.delete()
-      return;
-    }else if(message.attachments.first().height && message.attachments.first().width){
-      client.channels.cache.filter(channel => channel.topic == "==GLOBAL==").forEach((channel) =>{
-        const attachment = message.attachments.map(attachment => attachment.url)
-        channel.send({//添付ファイルあり(画像)
-          embeds:[{
-            color: message.member.displayHexColor,
-            author: {
-              name: message.author.tag,
-              icon_url: message.author.avatarURL()||"https://cdn.discordapp.com/embed/avatars/0.png",
-            },
-            description: message.content,
-            image: {
-              url: attachment[0]
-            },
-            footer: {
-              text: message.guild.name,
-              icon_url:message.guild.iconURL() ||"https://cdn.discordapp.com/embed/avatars/0.png"
-            },
-            timestamp: new Date()
-          }]}
-        );
+    });
+    message.delete()
+    return;
+  }else if(message.attachments.first().height && message.attachments.first().width){
+    client.channels.cache.filter(channel => channel.topic == "==GLOBAL==").forEach((channel) =>{
+      const attachment = message.attachments.map(attachment => attachment.url)
+      channel.send({//添付ファイルあり(画像)
+        embeds:[{
+          color: message.member.displayHexColor,
+          author: {
+            name: message.author.tag,
+            icon_url: message.author.avatarURL()||"https://cdn.discordapp.com/embed/avatars/0.png",
+          },
+          description: message.content,
+          image: {
+            url: attachment[0]
+          },
+          footer: {
+            text: message.guild.name,
+            icon_url:message.guild.iconURL() ||"https://cdn.discordapp.com/embed/avatars/0.png"
+          },
+          timestamp: new Date()
+        }]
       });
-      message.delete()
-      return;
-    }else{
-      client.channels.cache.filter(channel => channel.topic == "==GLOBAL==").forEach((channel) =>{
-        const attachment = message.attachments.map(attachment => attachment.url)
-        channel.send({//添付ファイルあり(画像以外)
-          embeds:[{
-            color: message.member.displayHexColor,
-            author: {
-              name: message.author.tag,
-              icon_url: message.author.avatarURL()||"https://cdn.discordapp.com/embed/avatars/0.png",
-            },
-            description: message.content,
-            footer: {
-              text: message.guild.name,
-              icon_url:message.guild.iconURL() ||"https://cdn.discordapp.com/embed/avatars/0.png"
-            },
-            fields: [
-              {
-                name: "**添付ファイル**",
-                value: `${attachment[0]}`
-              }
-            ],
-            timestamp: new Date()
-          }]}
-        );
+    });
+    message.delete()
+    return;
+  }else{
+    client.channels.cache.filter(channel => channel.topic == "==GLOBAL==").forEach((channel) =>{
+      const attachment = message.attachments.map(attachment => attachment.url)
+      channel.send({//添付ファイルあり(画像以外)
+        embeds:[{
+          color: message.member.displayHexColor,
+          author: {
+            name: message.author.tag,
+            icon_url: message.author.avatarURL()||"https://cdn.discordapp.com/embed/avatars/0.png",
+          },
+          description: message.content,
+          footer: {
+            text: message.guild.name,
+            icon_url:message.guild.iconURL() ||"https://cdn.discordapp.com/embed/avatars/0.png"
+          },
+          fields: [
+            {
+              name: "**添付ファイル**",
+              value: `${attachment[0]}`
+            }
+          ],
+          timestamp: new Date()
+        }]
       });
-      message.delete()
-      return;
-    }
+    });
+    message.delete()
+    return;
+  }
 }
 
 module.exports = global
