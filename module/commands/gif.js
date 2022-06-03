f1b57c247adac91524416ae3186d2503cdecdcc5
Taka005 <01taka005@gmail.com>
2022-06-03 12:12:42 +0900

    修正

--- module/commands/gif.js
+++ module/commands/gif.js
@@ -1,9 +1,10 @@
 async function gif(message){
   const config = require("../../config.json");
   const fetch = require("node-fetch");
+  require("dotenv").config();
   if(message.content.startsWith(`${config.prefix}gif`)){
     const gif = message.content.slice(5);
-    const gif_response = await fetch(`https://g.tenor.com/v1/search?q=${query}&key=${process.env.GIF_KEY}&limit=1&media_filter=minimal`);
+    const gif_response = await fetch(`https://g.tenor.com/v1/search?q=${gif}&key=${process.env.GIF_KEY}&limit=1&media_filter=minimal`);
     const gif_data = await gif_response.json();
     message.reply(gif_data.results[0].media[0].gif.url)
       .catch(()=>message.reply("GIFの取得に失敗しました..."))
