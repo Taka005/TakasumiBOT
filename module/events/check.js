765e89574e38b32d421a463d7a6e98949cdf1ced
Taka005 <01taka005@gmail.com>
2022-06-05 00:25:43 +0900

    修正

--- module/events/check.js
+++ module/events/check.js
@@ -3,12 +3,13 @@ async function check(interaction){
   if(interaction.customId.startsWith("check_")){
     const list = interaction.customId.split("_");
     const code = interaction.fields.getTextInputValue('code');
+    if(!code || isNaN(code)) return interaction.reply({content: `入力欄は空白、又は数字以外には出来ません`,ephemeral: true})
     if(code == list[2]){
       await interaction.member.roles.add(list[1])
         .then(()=>{interaction.reply({content: "認証しました",ephemeral: true})})
         .catch(()=>{interaction.reply({content: "認証に失敗しました...\nbotの権限、またはロールがしっかりと設定されてることを確認してください\n原因が解決できない場合はサポートサーバーまでよろしくお願いします\nhttps://discord.gg/GPs3npB63m",ephemeral: true})})
     }else{
-      await interaction.reply({content: `入力したコードが間違っています[答え:${count}]`,ephemeral: true})
+      await interaction.reply({content: `入力したコードが間違っています[答え:${list[2]}]`,ephemeral: true})
     }
     return;
   }
