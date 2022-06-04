46c0dd96220f2127529ddb1b1458962fbc12c422
Taka005 <01taka005@gmail.com>
2022-06-04 21:15:24 +0900

    help変更

--- module/commands/help.js
+++ module/commands/help.js
@@ -23,6 +23,10 @@ async function help(interaction){
             value:">auth [メンションかロールID]で認証機能が使えます"
           },
           {
+            name:"**>panel**",
+            value:">panel [メンションかロールID]で>authよりも強力な認証機能が使えます"
+          },
+          {
             name:"**>global**",
             value:"グローバルチャットに接続します。\n切断したい時はもう一回実行してください\n※他のグローバルチャットと競合する可能性があります"
           },
