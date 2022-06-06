1962798b912f7797fba2c6111f74bbd0cd320708
Taka005 <01taka005@gmail.com>
2022-06-06 16:13:06 +0900

    URL追加

---
+++ module/events/urlsecure.js
@@ -0,0 +1,9 @@
+async function urlsecure(message){
+  const fetch = require("node-fetch");
+  if(message.content.match("https://") || message.content.match("http://")){
+    
+
+  }
+}
+
+module.exports = urlsecure
