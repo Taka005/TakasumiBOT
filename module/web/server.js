af021a6afc6dbed4723ff001e5dddec9e0824b9f
Taka005 <01taka005@gmail.com>
2022-06-06 16:54:25 +0900

    修正

--- module/web/server.js
+++ module/web/server.js
@@ -14,12 +14,14 @@ async function server(client){
     res.redirect("https://discord.gg/GPs3npB63m");
     let time = new Date();
     console.info(`\x1b[34m[${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}]INFO: [${req.ip}]からsupportにリダイレクト`);
+    res.end()
   });
 
   app.get('/invite', (req, res) =>{
     res.redirect("https://discord.com/api/oauth2/authorize?client_id=981314695543783484&permissions=1644971949559&scope=bot%20applications.commands");
     let time = new Date();
     console.info(`\x1b[34m[${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}]INFO: [${req.ip}]からinviteにリダイレクト`);
+    res.end()
   });
   //------リダイレクト------//
 
@@ -48,6 +50,7 @@ async function server(client){
     });
     
     console.info(`\x1b[34m[${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}]INFO: [${req.ip}]からAPIにリクエスト`)
+    res.end()
   });
 
   //------API------//
@@ -56,16 +59,19 @@ async function server(client){
   app.get('/url/:name', (req, res) =>{
     if(!url[req.params.name]) return res.send(`<h1>NOT REGISTERED</h1>`);
     res.redirect(url[req.params.name]);
+    res.end()
   });
   //------短縮URL------//
 
   //------ERROR処理------//
   app.use((req, res, next)=>{
     res.status(404).send(`<h1>404 NOT FOUND</h1><br>[${req.path}]`);
+    res.end()
   });
 
   app.use(function(err, req, res, next){
     res.status(500).send(`<h1>500 SERVER ERROR</h1><br>[${err}]`);
+    res.end()
   });
   //------ERROR------
 }
