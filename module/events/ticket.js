127002d30bcdabcc2edc7525fa1f65d2a14a5c92
Taka005 <01taka005@gmail.com>
2022-06-10 22:40:27 +0900

    修正

--- module/events/ticket.js
+++ module/events/ticket.js
@@ -3,14 +3,14 @@ async function ticket(interaction){
     if(interaction.customId === "ticket"){
       const user = interaction.user.id
         if(interaction.guild.channels.cache.find(name => name.name === user)) return interaction.reply({content: "チケットは既に作成済みです", ephemeral: true});
-        const channel = interaction.guild.channels.cache.find(name => name.name === "ticket")
-        if(!channel) return interaction.reply({content:"ticketカテゴリーが存在しない為、チケットを作成できませんでした", ephemeral: true});
+        const ch = interaction.guild.channels.cache.find(name => name.name === "ticket")
+        if(!ch) return interaction.reply({content:"ticketカテゴリーが存在しない為、チケットを作成できませんでした", ephemeral: true});
         interaction.guild.channels.create(user, {
           permissionOverwrites: [{
             id: interaction.guild.roles.everyone,
             deny: ['VIEW_CHANNEL']
           }],
-          parent: channel.id
+          parent: ch.id
         })
           .then(channels => {
             channels.permissionOverwrites.edit(interaction.user.id, {VIEW_CHANNEL: true});
@@ -25,12 +25,10 @@ async function ticket(interaction){
             interaction.reply({content: `${channels}を作成しました`, ephemeral: true});
           })
           .catch(()=> interaction.reply({content:"チケットの作成に失敗しました...\nbotの権限、またはロールがしっかりと設定されてることを確認してください\n原因が解決できない場合はサポートサーバーまでよろしくお願いします\nhttps://discord.gg/GPs3npB63m", ephemeral:true}))
-      return;
     }
     if(interaction.customId === "close"){
       interaction.channel.delete()
         .catch(()=> interaction.reply({content:"チケットの削除に失敗しました...\nbotの権限、またはロールがしっかりと設定されてることを確認してください\n原因が解決できない場合はサポートサーバーまでよろしくお願いします\nhttps://discord.gg/GPs3npB63m", ephemeral:true}))
-      return;
     }
 }
   
