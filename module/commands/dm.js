72beba1b9e3cff109c37b44eb49884b639b45417
Taka005 <01taka005@gmail.com>
2022-06-15 18:03:57 +0900

    DM修正

--- module/commands/dm.js
+++ module/commands/dm.js
@@ -6,7 +6,7 @@ function dm(message,client){
     const args = message.content.split(" ").slice(1);
     if(!args[0]&&!args[1]) return message.reply(`${config.prefix}dm [userID] [TEXT]と指定してください`);  
     if(!args[0].match(/\d{18}/)) return message.reply("ユーザーIDは数字です")
-    client.users.cache.get(args[0]).send(`${args[1]}`)
+    client.users.cache.get(`${args[0]}`).send(`${args[1]}`)
       .then(()=>message.reply(`${args[0]}にDMを送信しました`))
       .catch(()=>message.reply("ユーザーのDMが有効になっていませんでした..."))
     return;
