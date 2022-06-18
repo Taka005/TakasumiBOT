253d1bb22e6388d94e581e5590d8dc9141996dbe
Taka005 <01taka005@gmail.com>
2022-06-18 23:15:14 +0900

    修正3

--- module/events/chat.js
+++ module/events/chat.js
@@ -1,42 +1,42 @@
 async function chat(message){
 if(!message.channel.type === 'GUILD_TEXT' || message.author.bot) return;  
- if(message.content.match(/(パー||ピー||プー||ペー||ポー)/)){
+ if(message.content.match(/(パー||ピー||プー||ペー||ポー)/g)){
    message.channel.send("ぺポーイ");
    return;
  }
- if(message.content.match(/(はー||ひー||ふー||へー||ほー)/)){
+ if(message.content.match(/(はー||ひー||ふー||へー||ほー)/g)){
     message.channel.send("ほはーい");
     return;
  }
- if(message.content.match(/(うぇ)/)){
+ if(message.content.match(/(うぇ)/g)){
     message.channel.send("うう..");
     return;
  }
- if(message.content.match(/(草)/)){
+ if(message.content.match(/(草)/g)){
     message.channel.send("HAHAH");
     return;
  }
- if(message.content.match(/(あー||いー)/)){
+ if(message.content.match(/(あー||いー)/g)){
    message.channel.sned("あーい");
    return;
  }
- if(message.content.match(/(www)/)){
+ if(message.content.match(/(www)/g)){
     message.channel.send("ワールド..ワイドウェブ?");
     return;
  }
- if(message.content.match(/(ぺぺ||ぽ？||ぴ？||ぺ？)/)){
+ if(message.content.match(/(ぺぺ||ぽ？||ぴ？||ぺ？)/g)){
     message.channel.send("ペポ？");
     return;
  }
- if(message.content.match(/(なあー||なあ〜||あー||あ〜)/)){
+ if(message.content.match(/(なあー||なあ〜||あー||あ〜)/g)){
     message.channel.send("そーい");
     return;
  }
- if(message.content.match(/(行く||いく)/)){
+ if(message.content.match(/(行く||いく)/g)){
     message.channel.send("どこいくペポ？");
     return;
  }
- if(message.content.match(/(酔う||酔った)/)){
+ if(message.content.match(/(酔う||酔った)/g)){
    message.channel.send("まーまー");
    return;
  }
