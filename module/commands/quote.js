39bbb9df6a033ada3056d8057899db8d80c855f7
Taka005 <01taka005@gmail.com>
2022-06-24 17:15:24 +0900

    quote修正

--- module/commands/quote.js
+++ module/commands/quote.js
@@ -6,25 +6,35 @@ async function quote(message){
   if(message.content === `${config.prefix}quote`){
     const reply = await message.fetchReference();
     if(!reply.content) return message.reply("メッセージの内容がありません");
-      const msg = await message.reply("画像生成中...")
-      const canvas = Canvas.createCanvas(1200, 630);
+      const msg = await message.reply("画像生成中...");
+
+      let fontSize = 50;
+      const applyText = (canvas, text) => {
+          const context = canvas.getContext('2d');
+          do {
+              context.font = `${fontSize / 2}px serif`;
+          } while (context.measureText(text).width > canvas.width - 300);
+          return context.font;
+      };
+      const canvas = Canvas.createCanvas(700, 250);
       const context = canvas.getContext('2d');
-      const background = await Canvas.loadImage('./images/quote.jpg');
+      const background = await Canvas.loadImage("./images/quote.jpg");
       context.drawImage(background, 0, 0, canvas.width, canvas.height);
+      context.strokeStyle = '#0099ff';
       context.strokeRect(0, 0, canvas.width, canvas.height);
-      //アバター
-      const avatar = await Canvas.loadImage(reply.author.avatarURL({format: 'jpg'}));
-      context.drawImage(avatar, 25, 25, 560, 560);
+      context.font = `${fontSize}px serif`;
+      context.fillStyle = '#ffffff';
+      context.fillText(reply.content, canvas.width / 2.5, canvas.height / 1.8);
+      context.font = applyText(canvas, reply.author.tag);
+      context.fillStyle = '#ffffff';
+      context.fillText(reply.author.tag, canvas.width / 2.5, canvas.height / 3.5);
       context.beginPath();
       context.arc(125, 125, 100, 0, Math.PI * 2, true);
       context.closePath();
       context.clip();
-      //内容
-      context.font = '70px sans-serif';
-      context.fillStyle = '#ffffff';
-      context.fillText(reply.content, canvas.width / 2.5, canvas.height / 2.5);
-      //名前
-      context.fillText(reply.author.tag, canvas.width / 2.5, canvas.height / 3.5);
+      const avatar = await Canvas.loadImage(reply.author.displayAvatarURL({ format: 'jpg' }));
+      context.drawImage(avatar, 25, 25, 200, 200);
+      context.drawImage(avatar, 25, 0, 200, canvas.height);
 
       const attachment = new MessageAttachment(canvas.toBuffer(), `quote-${reply.author.username}-TakasumiBOT.png`);
       msg.edit({ files: [attachment] })
