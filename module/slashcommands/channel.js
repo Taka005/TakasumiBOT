c4b6965be462a5b102b6d87f7a4f38323fb3920c
Taka005 <01taka005@gmail.com>
2022-07-02 16:59:11 +0900

    修正

--- module/slashcommands/channel.js
+++ module/slashcommands/channel.js
@@ -5,6 +5,7 @@ async function channel(interaction,client){
     const channel = await interaction.options.getChannel("channel");
 
     if(!interaction.member.permissions.has("MANAGE_MESSAGES")) return interaction.reply({ content:`channelを使うには「メッセージ管理」の権限が必要です`,ephemeral:true });
+    if(!channel.type === "GUILD_TEXT") return interaction.reply({ content:`テキストチャンネル以外は送信できません`,ephemeral:true })
     client.channels.cache.get(channel.id).send(`${text}`)
       .then(()=>interaction.reply({ content:`正常に送信しました`,ephemeral:true }))
       .catch(()=>interaction.reply({ content:`正常に送信が出来ませんでした`,ephemeral:true }))
