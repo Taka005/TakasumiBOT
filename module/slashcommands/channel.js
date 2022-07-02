7b4b23f11984f7a2c0ff06baa4fee35f79f8efe9
Taka005 <01taka005@gmail.com>
2022-07-02 19:38:45 +0900

    修正

--- module/slashcommands/channel.js
+++ module/slashcommands/channel.js
@@ -5,10 +5,22 @@ async function channel(interaction,client){
     const channel = await interaction.options.getChannel("channel");
 
     if(!interaction.member.permissions.has("MANAGE_MESSAGES")) return interaction.reply({ content:`channelを使うには「メッセージ管理」の権限が必要です`,ephemeral:true });
-    if(!channel.type === "GUILD_TEXT") return interaction.reply({ content:`テキストチャンネル以外は送信できません`,ephemeral:true })
-    await client.channels.cache.get(channel.id).send(`${text}`)
-      .then(()=>interaction.reply({ content:`正常に送信しました`,ephemeral:true }))
-      .catch(()=>interaction.reply({ content:`正常に送信が出来ませんでした`,ephemeral:true }))
+    try{
+      await client.channels.cache.get(channel.id).send(`${text}`)
+        .then(()=>interaction.reply({ content:`正常に送信しました`,ephemeral:true }))
+    }catch{
+      interaction.reply({
+        embeds:[{
+          author: {
+            name: "メッセージを正常に送信できませんでした",
+            icon_url: "https://taka.ml/images/error.jpg",
+          },
+          color: "RED",
+          description: `テキストチャンネルが指定されていないか、\nBOTの権限が不足しています`
+        }],
+        ephemeral:true
+      })
+    }
     return;
   }
 }
