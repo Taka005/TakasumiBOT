974e34d927ee4198bef6e3c31f1932e39a33e795
Taka005 <01taka005@gmail.com>
2022-07-11 22:26:08 +0900

    修正2

--- module/slashcommands/news.js
+++ module/slashcommands/news.js
@@ -1,28 +1,38 @@
 async function news(interaction){
+  async function sleep(waitSec, callback) {
+    setTimeout(callback, waitSec);
+  };
+
   const fetch = require("node-fetch");
   require("dotenv").config();
   if(!interaction.isCommand()) return;
   if(interaction.commandName === "news"){
     const news_response = await fetch(`https://newsapi.org/v2/top-headlines?country=jp&apiKey=${process.env.NEWS_KEY}`);
     const news_data = await news_response.json();
-    interaction.reply({
-      embeds:[{
-        author: {
-          name: news_data.articles[0].author,
-          icon_url: "https://taka.ml/images/success.jpg",
-        },
-        title: news_data.articles[0].title,
-        url: news_data.articles[0].url,
-        color: "WHITE",
-        description: news_data.articles[0].description,
-        image: {
-          url: news_data.articles[0].urlToImage
-        },
-        footer: {
-          text: `${news_data.articles[0].publishedAt}|${news_data.articles[0].source.name}`
-        },
-      }]
-    });
+
+    await interaction.deferReply()
+    for(let i=0;i<news_data.totalResults;i++){
+      sleep(5000,async function () {
+        await interaction.editReply({
+          embeds:[{
+            author: {
+              name: news_data.articles[i].author,
+              icon_url: "https://taka.ml/images/success.png",
+            },
+            title: news_data.articles[i].title,
+            url: news_data.articles[i].url,
+            color: "WHITE",
+            description: news_data.articles[i].description,
+            image: {
+              url: news_data.articles[i].urlToImage
+            },
+            footer: {
+              text: `${news_data.articles[i].publishedAt} | ${news_data.articles[i].source.name}`
+            },
+          }]
+        });
+      });
+    }
     return;
   }
 }
