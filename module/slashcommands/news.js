331761ffeb0fca828d56529b40e4f591f150adff
Taka005 <01taka005@gmail.com>
2022-07-11 23:30:12 +0900

    完成2

--- module/slashcommands/news.js
+++ module/slashcommands/news.js
@@ -8,7 +8,7 @@ async function news(interaction){
     const news_response = await fetch(`https://newsapi.org/v2/top-headlines?country=jp&apiKey=${process.env.NEWS_KEY}`);
     const news_data = await news_response.json();
 
-    for(let i=0;i<news_data.totalResults-1;i++){
+    for(let i=0;i<news_data.totalResults;i++){
       await interaction.editReply({
         embeds:[{
           title: news_data.articles[i].title,
@@ -24,7 +24,7 @@ async function news(interaction){
         }]
       }).catch(()=>{})
 
-      await setTimeout(4000);
+      await setTimeout(3500);
     }
     return;
   }
