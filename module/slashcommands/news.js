bacc58a2c0bf64651adf99fbc1383e508a19a15c
Taka005 <01taka005@gmail.com>
2022-07-11 22:33:44 +0900

    修正4

--- module/slashcommands/news.js
+++ module/slashcommands/news.js
@@ -1,33 +1,29 @@
 async function news(interaction){
-  async function sleep(waitSec, callback) {
-    setTimeout(callback, waitSec);
-  };
-
   const fetch = require("node-fetch");
   require("dotenv").config();
   if(!interaction.isCommand()) return;
   if(interaction.commandName === "news"){
+    await interaction.deferReply()
     const news_response = await fetch(`https://newsapi.org/v2/top-headlines?country=jp&apiKey=${process.env.NEWS_KEY}`);
     const news_data = await news_response.json();
 
-    await interaction.deferReply()
     for(let i=0;i<news_data.totalResults;i++){
-      sleep(8000,async function () {
+      setTimeout(async()=>{
         await interaction.editReply({
           embeds:[{
-            title: news_data.articles[i].title,
-            url: news_data.articles[i].url,
+            title: news_data.articles[i].title || null,
+            url: news_data.articles[i].url || null,
             color: "WHITE",
-            description: news_data.articles[i].description,
+            description: news_data.articles[i].description || null,
             image: {
-              url: news_data.articles[i].urlToImage
+              url: news_data.articles[i].urlToImage || null
             },
             footer: {
-              text: `${news_data.articles[i].publishedAt} | ${news_data.articles[i].source.name}`
+              text: `${news_data.articles[i].publishedAt || null} | ${news_data.articles[i].source.name || null}`
             },
           }]
         });
-      });
+      },5000);
     }
     return;
   }
