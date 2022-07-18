16cf4ddc0cbda1c3e141e8a7f72f25ec921d6fad
Taka005 <01taka005@gmail.com>
2022-07-18 22:46:26 +0900

    ページ変更

--- module/events/ready.js
+++ module/events/ready.js
@@ -13,29 +13,24 @@ async function ready(client){
     let s = now.getSeconds() 
       
     let stats = 0; 
-    setInterval(() => {
+    setInterval(()=>{
       if(stats == 0){
-        client.user.setActivity(`Created by Taka005#1203`, {
-          type: 'PLAYING'
-        });      
-        stats = 1;
-      }else if(stats == 1){
         client.user.setActivity(`/help || ping:${client.ws.ping}ms`, {
           type: 'PLAYING'
         });
-        stats = 2;
-      }else if(stats == 2){
+        stats = 1;
+      }else if(stats == 1){
         client.user.setActivity(`taka.ml || ver:${config.version}`, {
           type: 'PLAYING'
         });
-        stats = 3; 
-      }else if(stats == 3){
+        stats = 2; 
+      }else if(stats == 2){
         client.user.setActivity(`${client.guilds.cache.size}server || ${client.guilds.cache.map((g) => g.memberCount).reduce((a, c) => a + c)}user`,{
           type: 'PLAYING'
         });
         stats = 0;
       }
-    }, 8000)
+    },8000)
 
     client.channels.cache.get("947484748773736538").send(`BOT、API、WEBサーバーが再起動されました`);
 
