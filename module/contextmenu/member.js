c0becc583f090eaf90bead2a9193e7fecc7a0d65
Taka005 <01taka005@gmail.com>
2022-08-03 22:29:26 +0900

    修正3

--- module/contextmenu/member.js
+++ module/contextmenu/member.js
@@ -1,54 +1,55 @@
 async function member(interaction){
   if(!interaction.isContextMenu()) return;
-  const member = interaction.options.getMember("user");
+  if(interaction.commandName === "メンバー情報を表示"){
+    const member = interaction.options.getMember("user");
 
-  await interaction.reply({
-    embeds:[{
-      color: "GREEN",
-      author: {
-        name:`${member.user.tag}の検索結果`,
-        icon_url: "https://taka.ml/images/success.png"
-      },
-      timestamp: new Date(),
-      footer: {
-        text: "TakasumiBOT"
-      },
-      thumbnail: {
-        url: member.user.avatarURL({ format: 'png', dynamic: true, size: 1024 }) || "https://cdn.discordapp.com/embed/avatars/0.png"
-      },
-      fields: [
-        {
-          name: "**ID**",
-          value: `${member.id}`,
-          inline: true
+    await interaction.reply({
+      embeds:[{
+        color: "GREEN",
+        author: {
+          name:`${member.user.tag}の検索結果`,
+          icon_url: "https://taka.ml/images/success.png"
         },
-        {
-          name: "**ニックネーム**",
-          value: member.nickname||"未設定",
-          inline: true
+        timestamp: new Date(),
+        footer: {
+          text: "TakasumiBOT"
         },
-        {
-          name: "**作成日時**",
-          value: `${new Date(member.user.createdTimestamp).toLocaleDateString()}\n(${Math.round((Date.now() - member.user.createdAt) / 86400000)}日前)`,
-          inline: true
+        thumbnail: {
+          url: member.user.avatarURL({ format: 'png', dynamic: true, size: 1024 }) || "https://cdn.discordapp.com/embed/avatars/0.png"
         },
-        {
-          name:"**参加日時**",
-          value: `${new Date(member.joinedTimestamp).toLocaleDateString()}\n(${Math.round((Date.now() - member.joinedAt) / 86400000)}日前)`,
-          inline: true
-        },
-        {
-          name: "アカウントの種類",
-          value: member.user.bot ? "BOT" : "ユーザー",
-          inline: true
-        },
-        {
-          name:"**ロール**",
-          value: `${member.roles.cache.map(r => r).join('')}`
-        }
-      ]
-    }]
-  })
+        fields: [
+          {
+            name: "**ID**",
+            value: `${member.id}`,
+            inline: true
+          },
+          {
+            name: "**ニックネーム**",
+            value: member.nickname||"未設定",
+            inline: true
+          },
+          {
+            name: "**作成日時**",
+            value: `${new Date(member.user.createdTimestamp).toLocaleDateString()}\n(${Math.round((Date.now() - member.user.createdAt) / 86400000)}日前)`,
+            inline: true
+          },
+          {
+            name:"**参加日時**",
+            value: `${new Date(member.joinedTimestamp).toLocaleDateString()}\n(${Math.round((Date.now() - member.joinedAt) / 86400000)}日前)`,
+            inline: true
+          },
+          {
+            name: "アカウントの種類",
+            value: member.user.bot ? "BOT" : "ユーザー",
+            inline: true
+          },
+          {
+            name:"**ロール**",
+            value: `${member.roles.cache.map(r => r).join('')}`
+          }
+        ]
+      }]
+    })
     .catch((error)=>{
       interaction.reply({
         embeds:[{
@@ -62,6 +63,8 @@ async function member(interaction){
         ephemeral:true
       })
     });
+    return;
+  }
 }
 
 module.exports = member
