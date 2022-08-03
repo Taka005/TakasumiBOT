de18fbab8480b8a3a04cd6a53de1fed292541b14
Taka005 <01taka005@gmail.com>
2022-08-04 08:16:11 +0900

    修正

--- module/contextmenu/member.js
+++ module/contextmenu/member.js
@@ -1,13 +1,13 @@
 async function member(interaction){
-  if(!interaction.isContextMenu()) return;
+  if(!interaction.isUserContextMenuCommand()) return;
   if(interaction.commandName === "メンバー情報を表示"){
-    const member = interaction.options.getMember("user");
+    const info = interaction.targetMember;
 
     await interaction.reply({
       embeds:[{
         color: "GREEN",
         author: {
-          name:`${member.user.tag}の検索結果`,
+          name:`${info.user.tag}の検索結果`,
           icon_url: "https://taka.ml/images/success.png"
         },
         timestamp: new Date(),
@@ -15,12 +15,12 @@ async function member(interaction){
           text: "TakasumiBOT"
         },
         thumbnail: {
-          url: member.user.avatarURL({ format: 'png', dynamic: true, size: 1024 }) || "https://cdn.discordapp.com/embed/avatars/0.png"
+          url: info.user.avatarURL({ format: 'png', dynamic: true, size: 1024 }) || "https://cdn.discordapp.com/embed/avatars/0.png"
         },
         fields: [
           {
             name: "**ID**",
-            value: `${member.id}`,
+            value: `${info.user.id}`,
             inline: true
           },
           {
@@ -30,22 +30,22 @@ async function member(interaction){
           },
           {
             name: "**作成日時**",
-            value: `${new Date(member.user.createdTimestamp).toLocaleDateString()}\n(${Math.round((Date.now() - member.user.createdAt) / 86400000)}日前)`,
+            value: `${new Date(info.user.createdTimestamp).toLocaleDateString()}\n(${Math.round((Date.now() - info.user.createdAt) / 86400000)}日前)`,
             inline: true
           },
           {
             name:"**参加日時**",
-            value: `${new Date(member.joinedTimestamp).toLocaleDateString()}\n(${Math.round((Date.now() - member.joinedAt) / 86400000)}日前)`,
+            value: `${new Date(info.joinedTimestamp).toLocaleDateString()}\n(${Math.round((Date.now() - info.joinedAt) / 86400000)}日前)`,
             inline: true
           },
           {
             name: "アカウントの種類",
-            value: member.user.bot ? "BOT" : "ユーザー",
+            value: info.user.bot ? "BOT" : "ユーザー",
             inline: true
           },
           {
             name:"**ロール**",
-            value: `${member.roles.cache.map(r => r).join('')}`
+            value: `${info.roles.cache.map(r => r).join('')}`
           }
         ]
       }]
