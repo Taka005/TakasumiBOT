36f592895f705a9e40cf2f1db6614d6364314389
Taka005 <takanosumika.sub@gmail.com>
2022-08-13 15:55:28 +0900

    invite修正

--- module/events/invite.js
+++ module/events/invite.js
@@ -3,18 +3,18 @@ async function invite(guild){
   guild.channels.cache.map((channel) =>{
     if(find == 0){
       if(channel.type === "GUILD_TEXT"&&guild.me.permissionsIn(channel).has("VIEW_CHANNEL")&&guild.me.permissionsIn(channel).has("SEND_MESSAGES")){
-        return channel.send({
+        channel.send({
           embeds: [{
             color:"GREEN",
             thumbnail: {
               url: "https://taka.ml/images/bot.png"
             },
-            title:"BOT導入ありがとう!",
+            title:"BOT導入ありがとうございます!",
             description: "やっほー。TakasumiBOTだよ\n便利な機能を備えた万能BOTです\n\nグローバルチャット、カスタム埋め込み作成、認証機能いろいろあるよ!\nコマンドのhelpを表示する時は`/help`を実行してね\n\n質問がある？[サポートサーバー](https://discord.gg/GPs3npB63m)に入ってみてね",
             timestamp: new Date()
           }]
         });
-        find = 1;
+        return find = 1;
       }
     }
   });
