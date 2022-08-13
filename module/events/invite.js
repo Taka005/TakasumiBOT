9a674ade2c98ba624b1a8d48e796b1adbdacbcf9
Taka005 <01taka005@gmail.com>
2022-08-13 13:52:04 +0900

    修正2

--- module/events/invite.js
+++ module/events/invite.js
@@ -10,7 +10,7 @@ async function invite(guild){
               url: "https://taka.ml/images/bot.png"
             },
             title:"BOT導入ありがとう!",
-            description: "やっほー。TakasumiBOTだよ\n便利な機能を備えた万能BOTです\nグローバルチャット、カスタム埋め込み作成、認証機能いろいろあるよ!\nコマンドのhelpを表示する時は`/help`を実行してね\n\n質問がある？[サポートサーバー](https://discord.gg/GPs3npB63m)に入ってみてね\n[みんなの意見](https://taka.ml/feedback/)もお待ちしています!",
+            description: "やっほー。TakasumiBOTだよ\n便利な機能を備えた万能BOTです\n\nグローバルチャット、カスタム埋め込み作成、認証機能いろいろあるよ!\nコマンドのhelpを表示する時は`/help`を実行してね\n\n質問がある？[サポートサーバー](https://discord.gg/GPs3npB63m)に入ってみてね",
             timestamp: new Date()
           }]
         });
