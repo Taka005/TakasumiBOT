1d49f1d5a22438ab72a3087839cf1c54099d178f
Taka005 <01taka005@gmail.com>
2022-09-19 20:16:34 +0900

    修正

--- module/slashcommands/qrgen.js
+++ module/slashcommands/qrgen.js
@@ -8,9 +8,8 @@ async function qrgen(interaction){
     const qr_response = await fetch(`https://api.qrserver.com/v1/create-qr-code/?data=${encodeURI(text)}&size=256x256&format=png`)
       .then(res =>res.arrayBuffer()) 
       .catch(()=>{})
-    const attachment = new MessageAttachment(Buffer.from(qr_response), `QR:${text}`);
 
-    await interaction.reply({files:[attachment]});
+    await interaction.reply({files:[new MessageAttachment(qr_response.toBuffer(), `QR ${text}`)]});
       return;
   }
 }
