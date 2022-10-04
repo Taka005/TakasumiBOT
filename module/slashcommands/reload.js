c76f04c0261f1f0151677512c7ce982c6725b655
Taka005 <01taka005@gmail.com>
2022-10-04 18:47:04 +0900

    編集:reload.js 修正

--- module/slashcommands/reload.js
+++ module/slashcommands/reload.js
@@ -15,83 +15,92 @@ async function reload(interaction){
       ephemeral:true
     });
     
-    //commands
-    fs.readdir("./module/commands/", (err,files) =>{ 
-      files.forEach((file) =>{
-        if(!file.endsWith(`.js`)) return;
-        const event = require(`../commands/${file}`);
-        delete require.cache[require.resolve(`../commands/${file}`)];
+    try{
+      //commands
+      fs.readdir("./module/commands/", (err,files) =>{ 
+        files.forEach((file) =>{
+          if(!file.endsWith(`.js`)) return;
+          const event = require(`../commands/${file}`);
+          delete require.cache[require.resolve(`../commands/${file}`)];
+        });
       });
-    });
-    //ContextMenu
-    fs.readdir("./module/contextmenu/", (err,files) =>{ 
-      files.forEach((file) =>{
-        if(!file.endsWith(`.js`)) return;
-        const event = require(`../contextmenu/${file}`);
-        delete require.cache[require.resolve(`../contextmenu/${file}`)];
+      //ContextMenu
+      fs.readdir("./module/contextmenu/", (err,files) =>{ 
+        files.forEach((file) =>{
+          if(!file.endsWith(`.js`)) return;
+          const event = require(`../contextmenu/${file}`);
+          delete require.cache[require.resolve(`../contextmenu/${file}`)];
+        });
       });
-    });
-    //events
-    fs.readdir("./module/events/", (err,files) =>{ 
-      files.forEach((file) =>{
-        if(!file.endsWith(`.js`)) return;
-        const event = require(`../events/${file}`);
-        delete require.cache[require.resolve(`../events/${file}`)];
+      //events
+      fs.readdir("./module/events/", (err,files) =>{ 
+        files.forEach((file) =>{
+          if(!file.endsWith(`.js`)) return;
+          const event = require(`../events/${file}`);
+          delete require.cache[require.resolve(`../events/${file}`)];
+        });
       });
-    });
-    //functions
-    fs.readdir("./module/functions/", (err,files) =>{ 
-      files.forEach((file) =>{
-        if(!file.endsWith(`.js`)) return;
-        const event = require(`../functions/${file}`);
-        delete require.cache[require.resolve(`../functions/${file}`)];
+      //functions
+      fs.readdir("./module/functions/", (err,files) =>{ 
+        files.forEach((file) =>{
+          if(!file.endsWith(`.js`)) return;
+          const event = require(`../functions/${file}`);
+          delete require.cache[require.resolve(`../functions/${file}`)];
+        });
       });
-    });
-    //global
-    fs.readdir("./module/global/", (err,files) =>{ 
-      files.forEach((file) =>{
-        if(!file.endsWith(`.js`)) return;
-        const event = require(`../global/${file}`);
-        delete require.cache[require.resolve(`../global/${file}`)];
+      //global
+      fs.readdir("./module/global/", (err,files) =>{ 
+        files.forEach((file) =>{
+          if(!file.endsWith(`.js`)) return;
+          const event = require(`../global/${file}`);
+          delete require.cache[require.resolve(`../global/${file}`)];
+        });
       });
-    });
-    //slashcommands
-    fs.readdir("./module/slashcommands/", (err,files) =>{ 
-      files.forEach((file) =>{
-        if(!file.endsWith(`.js`)) return;
-        const event = require(`../slashcommands/${file}`);
-        delete require.cache[require.resolve(`../slashcommands/${file}`)];
+      //slashcommands
+      fs.readdir("./module/slashcommands/", (err,files) =>{ 
+        files.forEach((file) =>{
+          if(!file.endsWith(`.js`)) return;
+          const event = require(`../slashcommands/${file}`);
+          delete require.cache[require.resolve(`../slashcommands/${file}`)];
+        });
       });
-    });
-    //web
-    fs.readdir("./module/web/", (err,files) =>{ 
-      files.forEach((file) =>{
-        if(!file.endsWith(`.js`)) return;
-        const event = require(`../web/${file}`);
-        delete require.cache[require.resolve(`../web/${file}`)];
+      //web
+      fs.readdir("./module/web/", (err,files) =>{ 
+        files.forEach((file) =>{
+          if(!file.endsWith(`.js`)) return;
+          const event = require(`../web/${file}`);
+          delete require.cache[require.resolve(`../web/${file}`)];
+        });
       });
-    });
 
-    //その他
-    delete require.cache[require.resolve("../../config.json")];
-    delete require.cache[require.resolve("../../package.json")];
-    delete require.cache[require.resolve("../../package-lock.json")];
+      //その他
+      delete require.cache[require.resolve("../../config.json")];
+      delete require.cache[require.resolve("../../package.json")];
+      delete require.cache[require.resolve("../../package-lock.json")];
 
-    delete require.cache[require.resolve("../../data/api.json")];
-    delete require.cache[require.resolve("../../data/block_server.json")];
-    delete require.cache[require.resolve("../../data/block_user.json")];
-    delete require.cache[require.resolve("../../data/point.json")];
-    delete require.cache[require.resolve("../../data/url.json")];
+      delete require.cache[require.resolve("../../data/api.json")];
+      delete require.cache[require.resolve("../../data/block_server.json")];
+      delete require.cache[require.resolve("../../data/block_user.json")];
+      delete require.cache[require.resolve("../../data/point.json")];
+      delete require.cache[require.resolve("../../data/url.json")];
 
-    delete require.cache[require.resolve("../../data/global/main.json")];
-    delete require.cache[require.resolve("../../data/global/sub.json")];
+      delete require.cache[require.resolve("../../data/global/main.json")];
+      delete require.cache[require.resolve("../../data/global/sub.json")];
 
-    interaction.reply({
-      embeds:[{
-        color: "GREEN",
-        description: "リロードが完了しました",
-      }]
-    })
+      interaction.reply({
+        embeds:[{
+          color: "GREEN",
+          description: "リロードが完了しました",
+        }]
+      });
+    }catch(error){
+      interaction.reply({
+        embeds:[{
+          color: "RED",
+          description: `リロードに失敗しました\n\`\`\`${error}\`\`\``,
+        }]
+      });
+    }
   }
 }
 
