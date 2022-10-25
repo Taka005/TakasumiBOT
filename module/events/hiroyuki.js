ea201993a5eff9bf456b8d49b33d086117e05b30
Taka005 <takanosumika.main@gmail.com>
2022-10-25 11:48:14 +0000

    編集:hiroyuki.js

--- module/events/hiroyuki.js
+++ module/events/hiroyuki.js
@@ -43,8 +43,8 @@ async function hiroyuki(message,client){
 
     let content;
 
-    if(Object.keys(reply_1).filter(key=> message.content.match(key))){
-      content = Object.keys(sub).filter(key=> message.content.match(key))
+    if(Object.keys(reply_1).find(key=> message.content.match(key))){
+      content = Object.keys(reply_1).find(key=> message.content.match(key))
     }else if(message.content.match("@everyone")||message.content.match("@here")){
       content = random(["全体メンションする人って相当頭悪いんですよね…", "なんだろう、全体メンションするのやめてもらっていいですか？"])
     }else{
