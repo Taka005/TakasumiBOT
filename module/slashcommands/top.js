35bea527f44995cd287d656d8461e57f283f3091
Taka005 <takanosumika.main@gmail.com>
2022-10-28 00:06:02 +0000

    編集:top.js エラー修正

--- module/slashcommands/top.js
+++ module/slashcommands/top.js
@@ -2,6 +2,22 @@ async function top(interaction){
   const {MessageButton, MessageActionRow} = require("discord.js");
   if(!interaction.isCommand()) return;
   if(interaction.commandName === "top"){
+
+    if(
+      !interaction.guild.me.permissionsIn(interaction.channel).has("READ_MESSAGE_HISTORY")||
+      !interaction.guild.me.permissionsIn(interaction.channel).has("VIEW_CHANNEL")
+    ) return await interaction.reply({
+      embeds:[{
+        author: {
+          name: "BOTに権限がありません",
+          icon_url: "https://cdn.taka.ml/images/error.png",
+        },
+        color: "RED",
+        description: "この機能は、BOTに以下の権限が必要です\n```テキストチャンネルの閲覧\nメッセージ履歴を読む```\n何度も失敗する場合は[サポートサーバー](https://discord.gg/GPs3npB63m)まで、ご報告ください"
+      }],
+      ephemeral:true
+    });
+
     await interaction.deferReply();
     await interaction.editReply({
       embeds: [{
