563cb6355291c6330844e81e3360810739cc8596
Taka005 <01taka005@gmail.com>
2022-12-08 21:16:59 +0900

    修正:同期化

--- module/lib/sql.js
+++ module/lib/sql.js
@@ -1,6 +1,7 @@
 module.exports = async(query)=>{
   const mysql = require("mysql");
   require("dotenv").config();
+  const util = require("util");
 
   const connection = mysql.createConnection({
     host: "public.b5m4d.tky2.mdbs.jp",
@@ -8,6 +9,8 @@ module.exports = async(query)=>{
     password: process.env.DB_PASSWORD,
     database: "b5m4d_"
   });
+
+  connection.query = util.promisify(connection.query);
 
   const res = await connection.query(query);
   connection.end();
