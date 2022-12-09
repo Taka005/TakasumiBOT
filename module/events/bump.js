1c400b123e745f200a744d9603f7638a9a1a4fd4
Taka005 <01taka005@gmail.com>
2022-12-09 19:56:27 +0900

    修正:bump検知しないバグ訂正

--- module/events/bump.js
+++ module/events/bump.js
@@ -3,40 +3,40 @@ module.exports = async(message)=>{
 
   if(message.author.id === "302050872383242240"){
     if(
-      !message.embeds[0]?.description.match(/表示順をアップしたよ/)||
-      !message.embeds[0]?.description.match(/Bump done/)
-    ) return;
+      message.embeds[0]?.description.match(/表示順をアップしたよ/)||
+      message.embeds[0]?.description.match(/Bump done/)
+    ){
+      const data = await mysql(`SELECT * FROM bump WHERE server = ${message.guild.id} LIMIT 1;`);
+      await message.channel.send({
+        embeds:[{
+          color: "WHITE",
+          title:"BUMP通知",
+          description:"UPを受信しました\n2時間後に通知します"
+        }]  
+      }).catch(()=>{})
 
-    const data = await mysql(`SELECT * FROM bump WHERE server = ${message.guild.id} LIMIT 1;`);
-    await message.channel.send({
-      embeds:[{
-        color: "WHITE",
-        title:"BUMP通知",
-        description:"UPを受信しました\n2時間後に通知します"
-      }]  
-    }).catch(()=>{})
-
-    if(data){
-      setTimeout(async()=>{
-        await message.channel.send({
-          content: `<@&${data}>`,
-          embeds:[{
-            color: "WHITE",
-            title:"BUMP通知",
-            description:"BUMPの時間です\n`/bump`でサーバーの表示順位を上げよう！"
-          }]  
-        }).catch(()=>{})
-      },60000 * 120)
-    }else{
-      setTimeout(async()=>{
-        await message.channel.send({
-          embeds:[{
-            color: "WHITE",
-            title:"BUMP通知",
-            description:"BUMPの時間です\n`/bump`でサーバーの表示順位を上げよう！"
-          }]  
-        }).catch(()=>{})
-      },60000 * 120)
+      if(data){
+        setTimeout(async()=>{
+          await message.channel.send({
+            content: `<@&${data}>`,
+            embeds:[{
+              color: "WHITE",
+              title:"BUMP通知",
+              description:"BUMPの時間です\n`/bump`でサーバーの表示順位を上げよう！"
+            }]  
+          }).catch(()=>{})
+        },60000 * 120)
+      }else{
+        setTimeout(async()=>{
+          await message.channel.send({
+            embeds:[{
+              color: "WHITE",
+              title:"BUMP通知",
+              description:"BUMPの時間です\n`/bump`でサーバーの表示順位を上げよう！"
+            }]  
+          }).catch(()=>{})
+        },60000 * 120)
+      }
     }
   }
 }
--- module/events/dissoku.js
+++ module/events/dissoku.js
@@ -3,40 +3,40 @@ module.exports = async(message)=>{
 
   if(message.author.id === "761562078095867916"){
     if(
-      !message.embeds[0]?.fields[0].name.match(/をアップしたよ/)||
-      !message.embeds[0]?.fields[0].name.match(/I've bumped up/)
-    ) return;
+      message.embeds[0]?.fields[0].name.match(/をアップしたよ/)||
+      message.embeds[0]?.fields[0].name.match(/I've bumped up/)
+    ){
+      const data = await mysql(`SELECT * FROM dissoku WHERE server = ${message.guild.id} LIMIT 1;`);
+      await message.channel.send({
+        embeds:[{
+          color: "BLUE",
+          title:"UP通知",
+          description:"UPを受信しました\n1時間後に通知します"
+        }]  
+      }).catch(()=>{})
 
-    const data = await mysql(`SELECT * FROM dissoku WHERE server = ${message.guild.id} LIMIT 1;`);
-    await message.channel.send({
-      embeds:[{
-        color: "BLUE",
-        title:"UP通知",
-        description:"UPを受信しました\n1時間後に通知します"
-      }]  
-    }).catch(()=>{})
-
-    if(data){
-      setTimeout(async()=>{
-        await message.channel.send({
-          content: `<@&${data}>`,
-          embeds:[{
-            color: "BLUE",
-            title:"UP通知",
-            description:"DISSOKUの時間です\n`/dissoku up`でサーバーの表示順位を上げよう！"
-          }]  
-        }).catch(()=>{})
-      },60000 * 60)
-    }else{
-      setTimeout(async()=>{
-        await message.channel.send({
-          embeds:[{
-            color: "BLUE",
-            title:"UP通知",
-            description:"DISSOKUの時間です\n`/dissoku up`でサーバーの表示順位を上げよう！"
-          }]  
-        }).catch(()=>{})
-      },60000 * 60)
+      if(data){
+        setTimeout(async()=>{
+          await message.channel.send({
+            content: `<@&${data}>`,
+            embeds:[{
+              color: "BLUE",
+              title:"UP通知",
+              description:"DISSOKUの時間です\n`/dissoku up`でサーバーの表示順位を上げよう！"
+            }]  
+          }).catch(()=>{})
+        },60000 * 60)
+      }else{
+        setTimeout(async()=>{
+          await message.channel.send({
+            embeds:[{
+              color: "BLUE",
+              title:"UP通知",
+              description:"DISSOKUの時間です\n`/dissoku up`でサーバーの表示順位を上げよう！"
+            }]  
+          }).catch(()=>{})
+        },60000 * 60)
+      }
     }
   }
 }
