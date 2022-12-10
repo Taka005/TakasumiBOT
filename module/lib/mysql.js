d994cdc6f653ef8dfe35d862a3dda0f1dfb704ae
Taka005 <takanosumika.sub@gmail.com>
2022-12-10 10:47:29 +0900

    修正:DB接続

--- module/lib/mysql.js
+++ module/lib/mysql.js
@@ -1,18 +1,17 @@
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host: "public.b5m4d.tky2.mdbs.jp",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "b5m4d_"
+});  
+
 module.exports = async(query)=>{
-  const mysql = require("mysql");
   require("dotenv").config();
   const util = require("util");
 
-  const connection = mysql.createConnection({
-    host: "public.b5m4d.tky2.mdbs.jp",
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: "b5m4d_"
-  });  
-
   connection.query = util.promisify(connection.query);
   const res = await connection.query(query);
-  connection.end();
 
   return res;
 }
