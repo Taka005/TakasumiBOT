668588a615d2cf2ef84630f3866add5c399c38f9
Taka005 <01taka005@gmail.com>
2022-12-11 10:42:53 +0900

    追加:pin

---
+++ module/contextmenu/pin.js
@@ -0,0 +1,82 @@
+module.exports = async(interaction)=>{
+  const mysql = require("../lib/mysql.js");
+  if(!interaction.isContextMenu()) return;
+  if(interaction.commandName === "メッセージをピン留め"){
+    const message = await interaction.options.getMessage("message");
+    if(!message.content) return await interaction.reply({
+      embeds:[{
+        author: {
+          name: "メッセージをピン留めできませんでした",
+          icon_url: "https://cdn.taka.ml/images/system/error.png",
+        },
+        color: "RED",
+        description: "メッセージの内容が存在しません"
+      }],
+      ephemeral:true
+    });
+
+    if(
+      !interaction.guild.me.permissionsIn(interaction.channel).has("VIEW_CHANNEL")||
+      !interaction.guild.me.permissionsIn(interaction.channel).has("SEND_MESSAGES")||
+      !interaction.guild.me.permissionsIn(interaction.channel).has("MANAGE_MESSAGES")||
+      !interaction.guild.me.permissionsIn(interaction.channel).has("MANAGE_CHANNELS")
+    ) return await interaction.reply({
+      embeds:[{
+        author: {
+          name: "BOTに権限がありません",
+          icon_url: "https://cdn.taka.ml/images/system/error.png",
+        },
+        color: "RED",
+        description: "グローバルチャットは、BOTに以下の権限が必要です\n```チャンネルの閲覧\nメッセージを送信\nメッセージの管理\nチャンネルの管理```"
+      }],
+      ephemeral:true
+    });
+
+    if(
+      !interaction.member.permissions.has("MANAGE_CHANNELS")||
+      !interaction.member.permissions.has("MANAGE_MESSAGES")
+    ) return await interaction.reply({
+      embeds:[{
+        author: {
+          name: "権限がありません",
+          icon_url: "https://cdn.taka.ml/images/system/error.png",
+        },
+        color: "RED",
+        description: "このコマンドを実行するには、あなたがこのサーバーで以下の権限を持っている必要があります\n```ロールの管理\nチャンネルの管理```"
+      }],
+      ephemeral:true
+    });
+      
+    const data = await mysql(`SELECT * FROM pin WHERE channel = ${message.channel.id} LIMIT 1;`);
+    if(data[0]){
+      return await interaction.reply({
+        embeds:[{
+          author: {
+            name: "メッセージをピン留めできませんでした",
+            icon_url: "https://cdn.taka.ml/images/system/error.png",
+          },
+          color: "RED",
+          description: "既にこのチャンネルにはピン留めされたメッセージが存在します\nピン留めの解除は送信された埋め込みを削除してください"
+        }],
+        ephemeral:true
+      });
+    }
+
+    const msg = await interaction.reply({
+      embeds:[{
+        color: "GREEN",
+        author: {
+          name: message.author.tag,
+          icon_url: message.author.avatarURL()||"https://cdn.discordapp.com/embed/avatars/0.png",
+        },
+        description: message.content,
+        footer: {
+          text:"TakasumiBOT PIN"
+        }
+      }],
+      fetchReply: true
+    });
+
+    await mysql(`INSERT INTO pin (channel, message, limit) VALUES("${message.channel.id}","${message.id}","1");`);
+  }
+}
