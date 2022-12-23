65e8ec2a55a8c04b59fa7a1bd35983ae88c22399
Taka005 <01taka005@gmail.com>
2022-12-23 14:28:19 +0900

    修正:モデレート

--- module/events/message/moderate.js
+++ module/events/message/moderate.js
@@ -1,4 +1,4 @@
-//const time = [];
+const time = [];
 
 module.exports = async(message)=>{
   const ngword = require("../../../file/moderate/ngword.json");
@@ -6,9 +6,25 @@ module.exports = async(message)=>{
 
   const data = await mysql(`SELECT * FROM moderate WHERE id = ${message.guild.id} LIMIT 1;`);
   if(data[0]){
-    message.delete().catch(()=>{});
     if(data[0].type === "high"){
-      if(ngword.includes(message.content)){
+      //文字数制限
+      if(message.content.length > 1000){
+        message.delete().catch(()=>{});
+        return message.channel.send({
+          embeds:[{
+            author: {
+              name: "自動モデレート",
+              icon_url: "https://cdn.taka.ml/images/system/warn.png"
+              },
+              description: "メッセージの文字数が多すぎたため、メッセージを削除しました",
+              timestamp: new Date(),
+              color: "YELLOW"
+            }]
+        }).catch(()=>{})
+      }
+      //NGワード検知
+      if(ngword.high.find(e=>e.includes(message.content))){
+        message.delete().catch(()=>{});
         return message.channel.send({
           embeds:[{
             author: {
@@ -22,7 +38,24 @@ module.exports = async(message)=>{
         }).catch(()=>{})
       }
     }else if(data[0].type === "normal"){
-      if(ngword.includes(message.content)){
+      //文字数制限
+      if(message.content.length > 1300){
+        message.delete().catch(()=>{});
+        return message.channel.send({
+          embeds:[{
+            author: {
+              name: "自動モデレート",
+              icon_url: "https://cdn.taka.ml/images/system/warn.png"
+              },
+              description: "メッセージの文字数が多すぎたため、メッセージを削除しました",
+              timestamp: new Date(),
+              color: "YELLOW"
+            }]
+        }).catch(()=>{})
+      }
+      //NGワード検知
+      if(ngword.low.find(e=>e.includes(message.content))){
+        message.delete().catch(()=>{});
         return message.channel.send({
           embeds:[{
             author: {
@@ -36,19 +69,22 @@ module.exports = async(message)=>{
         }).catch(()=>{})
       }
     }else if(data[0].type === "low"){
-      if(ngword.includes(message.content)){
+      //文字数制限
+      if(message.content.length > 1800){
+        message.delete().catch(()=>{});
         return message.channel.send({
           embeds:[{
             author: {
               name: "自動モデレート",
               icon_url: "https://cdn.taka.ml/images/system/warn.png"
-            },
-            description: "NGワードを検知したため、メッセージを削除しました",
-            timestamp: new Date(),
-            color: "YELLOW"
-          }]
+              },
+              description: "メッセージの文字数が多すぎたため、メッセージを削除しました",
+              timestamp: new Date(),
+              color: "YELLOW"
+            }]
         }).catch(()=>{})
       }
+    
     }
   }
 }
