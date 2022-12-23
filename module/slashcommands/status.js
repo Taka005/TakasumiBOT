0a8275c6512ff96199c5be92892a5d7b92113d57
Taka005 <01taka005@gmail.com>
2022-12-23 19:23:20 +0900

    修正:ミス2

--- module/slashcommands/status.js
+++ module/slashcommands/status.js
@@ -1,5 +1,6 @@
 module.exports = async(interaction,client)=>{
   const os = require("os");
+  const fetch = require("node-fetch");
   const mysql = require("../lib/mysql");
   const global = require("../../data/global/main.json");
   const hiroyuki = require("../../data/hiroyuki/main.json");
