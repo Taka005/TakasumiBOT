9d61d03689ff7fb5365d9f99240d6075dafac7fe
Taka005 <01taka005@gmail.com>
2023-01-10 21:34:08 +0900

    修正:テスト

--- module/events/interaction/script_event.js
+++ module/events/interaction/script_event.js
@@ -25,7 +25,7 @@ module.exports = async(interaction)=>{
     })
       .then(res=>res.json())
       .catch(()=>{})
-
+console.log(res)
     if(res.status === "0"){
       await interaction.reply({
         embeds:[{
