62c14b2c12d88aa91d2d8f57c8fbef4f31bca8db
Taka005 <01taka005@gmail.com>
2023-01-24 10:57:47 +0900

    fix:ステータス変更

--- module/events/status.js
+++ module/events/status.js
@@ -9,17 +9,12 @@ module.exports = async(client)=>{
       });
       stats = 1;
     }else if(stats === 1){
-      client.user.setActivity(`taka.ml || ver:${package.version}`,{
-        type: "PLAYING"
-      });
-      stats = 2; 
-    }else if(stats === 2){
       client.user.setActivity(`${client.guilds.cache.size}server || ${client.guilds.cache.map((g)=>g.memberCount).reduce((a,c)=>a+c)}user`,{
         type: "PLAYING"
       });
       stats = 0;
     }
-  },6000)
+  },5000)
 
   client.channels.cache.get("947484748773736538").send("BOT、APIサーバーが再起動されました");
 
