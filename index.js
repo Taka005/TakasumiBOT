ff0106e0414a4866845b5645e08c15ad7575cf79
Taka005 <takanosumika.sub@gmail.com>
2023-01-27 16:35:32 +0900

    fix:シャード

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const client = new Client({
     Intents.FLAGS.GUILD_INVITES,
     Intents.FLAGS.GUILD_MESSAGES
   ],
-  shards: "auto"
+  shardCount: "2"
 });
 
 console.log("\x1b[32m*******************************");
