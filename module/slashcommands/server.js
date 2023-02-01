9c7d304142379f9652ec8779ed1719f94aef61da
Taka005 <01taka005@gmail.com>
2023-02-01 17:51:13 +0900

    fix:日付表示変更

--- module/slashcommands/server.js
+++ module/slashcommands/server.js
@@ -33,7 +33,7 @@ module.exports = async(interaction)=>{
           },
           {
             name: "作成日時",
-            value: `${new Date(interaction.guild.createdTimestamp).toLocaleDateString()}\n(${Math.round((Date.now() - interaction.guild.createdAt) / 86400000)}日前)`
+            value: `${new Date(interaction.guild.createdTimestamp).toLocaleString()}\n(${Math.round((Date.now() - interaction.guild.createdAt) / 86400000)}日前)`
           },
           {
             name: "統計情報",
