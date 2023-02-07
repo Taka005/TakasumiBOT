95deadd71904645fdd7c5ca75d8a651ce7db277e
Taka005 <takanosumika.sub@gmail.com>
2023-02-07 10:56:24 +0900

    fix:ミス

--- module/commands/exec.js
+++ module/commands/exec.js
@@ -4,15 +4,15 @@ module.exports = async(message,client)=>{
   if(message.content.startsWith(`${config.prefix}exec`)){
     if(message.author.id !== `${config.admin}`) return message.reply("このコマンドは製作者専用です");
     if(message.content === `${config.prefix}exec`) return message.reply("実行するコードが必要です");
-      const code = message.content.slice(6);
-      const script = `module.exports = async(message,client)=>{\n  ${code}\n}`;
-      try{
-        fs.writeFileSync("./tmp/script.js",script,"utf8");
-        const run = require("../../tmp/script");
-        run(message,client);
-      }catch(error){
-        return message.reply(`実行中にエラーが発生しました[${error}]`);
-      }
-      delete require.cache[require.resolve("../../tmp/script")];
+    
+    const script = `module.exports = async(message,client)=>{\n  ${message.content.slice(6)}\n}`;
+    try{
+      fs.writeFileSync("./tmp/script.js",script,"utf8");
+      const run = require("../../tmp/script");
+      run(message,client);
+    }catch(error){
+      return message.reply(`実行中にエラーが発生しました[${error}]`);
+    }
+    delete require.cache[require.resolve("../../tmp/script")];
     }
 }
