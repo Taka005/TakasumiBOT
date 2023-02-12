1b0e355d2199b5ecef92a7ac3a718dd1c98c4d3f
Taka005 <01taka005@gmail.com>
2023-02-12 19:10:18 +0900

    fix:ミス

--- module/commands/exec.js
+++ module/commands/exec.js
@@ -2,7 +2,7 @@ module.exports = async(message,client)=>{
   const fs = require("fs");
   const config = require("../../config.json");
   if(message.content.startsWith(`${config.prefix}exec`)){
-    if(message.author.id !== `${config.admin}`) return message.reply("このコマンドは製作者専用です");
+    if(message.author.id !== `${config.admin}`) return message.reply("このコマンドは関係者専用です");
     if(message.content === `${config.prefix}exec`) return message.reply("実行するコードが必要です");
     
     const script = `module.exports = async(message,client)=>{\n  ${message.content.slice(6)}\n}`;
@@ -11,7 +11,7 @@ module.exports = async(message,client)=>{
       const run = require("../../tmp/script");
       run(message,client);
     }catch(error){
-      return message.reply(`実行中にエラーが発生しました[${error}]`);
+      message.reply(`実行中にエラーが発生しました[${error}]`);
     }
     delete require.cache[require.resolve("../../tmp/script")];
     }
