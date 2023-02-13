ecb6319476269d463f91460201205fe003b046e6
Taka005 <01taka005@gmail.com>
2023-02-13 22:20:46 +0900

    fix:調整

--- module/events/message/open.js
+++ module/events/message/open.js
@@ -12,7 +12,7 @@ module.exports = async(message,client)=>{
     const url = message.content.match(/(https?:\/\/(?:ptb\.|canary\.)?(?:discord|discordapp)\.com\/channels\/)(\d{18,19}\/\d{18,19}\/\d{18,19})/);
 
     const ignore = await mysql(`SELECT * FROM \`ignore\` WHERE id = ${message.guild.id} LIMIT 1;`);
-    if(ignore[0]||limit(message.guild.id,message)) return;
+    if(ignore[0]||limit(message)) return;
 
     const id = url[2].split("/");
     const channel = client.channels.cache.get(id[1]);
