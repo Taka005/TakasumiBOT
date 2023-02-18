1ca8409ad5add902e202f64645bff0d4de8b7648
Taka005 <01taka005@gmail.com>
2023-02-18 12:44:39 +0900

    fix:エラーミス

--- module/commands/exec.js
+++ module/commands/exec.js
@@ -2,16 +2,16 @@ module.exports = async(message,client)=>{
   const fs = require("fs");
   const config = require("../../config.json");
   if(message.content.startsWith(`${config.prefix}exec`)){
-    if(message.author.id !== `${config.admin}`) return message.reply("このコマンドは関係者専用です");
-    if(message.content === `${config.prefix}exec`) return message.reply("実行するコードが必要です");
+    if(message.author.id !== `${config.admin}`) return message.reply("このコマンドは関係者専用です").catch(()=>{});
+    if(message.content === `${config.prefix}exec`) return message.reply("実行するコードが必要です").catch(()=>{});
     
     const script = `module.exports = async(message,client)=>{\n  ${message.content.slice(6)}\n}`;
     try{
       fs.writeFileSync("./tmp/script.js",script,"utf8");
       const run = require("../../tmp/script");
-      run(message,client);
+      await run(message,client);
     }catch(error){
-      message.reply(`実行中にエラーが発生しました[${error}]`);
+      message.reply(`実行中にエラーが発生しました[${error}]`).catch(()=>{});
     }
     delete require.cache[require.resolve("../../tmp/script")];
     }
