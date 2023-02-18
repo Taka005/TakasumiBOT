6cd688e448082cfb4f564562a43c7d16987bd282
Taka005 <01taka005@gmail.com>
2023-02-18 23:46:38 +0900

    style:調整

--- module/auth/auth.js
+++ module/auth/auth.js
@@ -4,13 +4,13 @@ module.exports = async(interaction)=>{
     await interaction.reply({
         embeds:[{
         author: {
-            name: "認証できません",
-            icon_url: "https://cdn.taka.ml/images/system/error.png",
+          name: "認証できません",
+          icon_url: "https://cdn.taka.ml/images/system/error.png",
         },
         color: "RED",
         description: "この認証は現在使用不可です\n認証機能は`/auth`に統合しました"
         }],
-        ephemeral:true
+        ephemeral: true
     });
   }
 }
