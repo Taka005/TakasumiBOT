c2929624f3dd0daaf20e1b54c4cef0519d0e81df
Taka005 <takanosumika.main@gmail.com>
2023-04-01 15:40:02 +0900

    fix:調整

--- module/slashcommands/nslookup.js
+++ module/slashcommands/nslookup.js
@@ -8,21 +8,34 @@ module.exports = async(interaction)=>{
     await interaction.deferReply();
     try{
       const data = await fetch(`https://dns.google/resolve?name=${name}&type=${type}`)
-        .then(res=>res.json())
+        .then(res=>res.json());
+
+      if(!data.Answer) return await interaction.editReply({
+        embeds:[{
+          author:{
+            name: "DNS情報が取得できませんでした",
+            icon_url: "https://cdn.taka.ml/images/system/error.png"
+          },
+          color: "RED",
+          description: "指定したタイプで取得出来ませんでした"
+        }],
+        ephemeral: true
+      });
 
       await interaction.editReply({
         embeds:[{
           author:{
-            name: "DNS情報を取得しました",
+            name: `${data.Question[0].name}レコードを取得しました`,
             icon_url: "https://cdn.taka.ml/images/system/success.png"
           },
           color: "GREEN",
           fields:[
-            {
-              name: "作者",
-              value: "なし",
-              inline: true
-            }
+            data.Answer.map(a=>(
+              {
+                name: "作者",
+                value: "なし"
+              }
+            ))
           ],
           footer:{
             text: "TakasumiBOT"
@@ -37,7 +50,7 @@ module.exports = async(interaction)=>{
             icon_url: "https://cdn.taka.ml/images/system/error.png"
           },
           color: "RED",
-          description: "名前解決ができませんでした"
+          description: "違うアドレスを試してください"
         }],
         ephemeral: true
       });
